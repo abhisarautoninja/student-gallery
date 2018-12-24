69fbb4ff217e8f118c5841e48e24f9651d26c1e9
Abhisar Tripathi <abhisaarsvnit@gmail.com>
2018-12-24 17:16:07 +0530

    Add files via upload

--- emb/src/StudentDetailPage.js
+++ emb/src/StudentDetailPage.js
@@ -2,15 +2,7 @@
 import React, { Component } from "react";
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-const data = [
-    {name: 'Page A', uv: 4000},
-    {name: 'Page B', uv: 3000},
-    {name: 'Page C', uv: 2000},
-    {name: 'Page D', uv: 2780},
-    {name: 'Page E', uv: 1890},
-    {name: 'Page F', uv: 2390},
-    {name: 'Page G', uv: 3490},
-];
+
 class StudentDetailPage extends Component {
   constructor() {
     super();
@@ -21,7 +13,8 @@ class StudentDetailPage extends Component {
     console.log(this.props)
   }
   componentDidMount() {
-    let message = JSON.parse(this.props.location.pathname.split("/")[this.props.location.pathname.split("/").length-1]);
+    console.log(this.props)
+    const message = JSON.parse(this.props.location.data);
     let data = [];
     Object.keys(message).map((key)=>{
         data.push({name: key, Marks:message[key]});
--- emb/src/StudentGallery.js
+++ emb/src/StudentGallery.js
@@ -8,7 +8,7 @@ const StudentGallery = ({data, getTotalMarks}) =>
     <Row className="show-grid">
     {Object.keys(data).map(key => (
         <Col xs={12} md={6} lg={4}>
-            <Link to={{pathname: `/student/${key}/${JSON.stringify(data[key].marks)}`}}>
+            <Link to={{pathname: `/student/${key}`, data: `${JSON.stringify(data[key].marks)}`}}>
                 <div className="card">
                     <div className="container">
                     <h4>
